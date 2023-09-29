a5075baa85f5993519c9b8fc7276dd5beec52620
Cristobal Rosa <crisrogar@gmail.com>
2023-09-29 10:47:05 +0200

    update northwindQueries.sql

--- northwindQueries.sql
+++ northwindQueries.sql
@@ -1,2 +1,6 @@
 -- Change field name in query using 'AS'
 SELECT FirstName AS Nombre, LastName AS Apellidos, Notes AS Notas FROM employees;
+
+-- Obtain a ordered list using 'ORDER BY'
+SELECT * FROM products ORDER BY Price ASC;
+SELECT * FROM products ORDER BY Price DESC;
