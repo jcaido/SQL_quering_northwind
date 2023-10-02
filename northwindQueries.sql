531a8ea0eed103d8eb70c878264bdbe764821ee0
Cristobal Rosa <crisrogar@gmail.com>
2023-10-02 10:08:17 +0200

    update northWindQueries.sql

--- northwindQueries.sql
+++ northwindQueries.sql
@@ -10,3 +10,7 @@ SELECT * FROM products WHERE ProductID = 38;
 -- Other queries with 'WHERE'
 SELECT * FROM products WHERE ProductName = 'Tofu';
 SELECT * FROM products WHERE Price < 40 ORDER BY Price ASC; 
+SELECT * FROM customers WHERE CustomerID >= 50 AND CustomerID < 55;
+SELECT * FROM customers WHERE City = 'Bruxelles' OR City = 'London';
+SELECT * FROM customers WHERE NOT City = 'London';
+SELECT * FROM products WHERE (Price < 20 OR CategoryID = 6) AND SupplierID = 7;
